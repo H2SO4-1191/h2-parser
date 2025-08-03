db7e06bb9721258b064d1c54e91688e288e00b20
Mustafa Muhammad <h2so4.1191@gmail.com>
2025-08-03 13:17:10 +0300

    Repo Management Phase: included some edited files

--- src/Main.java
+++ src/Main.java
@@ -15,7 +15,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 import java.util.Scanner;
-
 public class Main {
     static JFrame mainFrame;
     static JPanel backgroundPanel;
